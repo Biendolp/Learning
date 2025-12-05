42d4bb1ca44edd9a638ee29ba2f02fdffa93a863
Biendolp <brendanphilippe7@gmail.com>
2025-12-04 22:04:00 -0600

    Learning Objects
    Added movie object and a few functions to understand objects

--- JS-Fundamentals.js
+++ JS-Fundamentals.js
@@ -48,3 +48,57 @@ stringFour = stringThree.split("").reverse().join("")
 //.join("") Turns the indexed items in the array together into one, starting from index 0 -> the end
 
 console.log("Reversed String 2: ", stringFour)
+console.log("")
+
+
+// Learning Objects
+
+let movie = {
+  title: "",
+  director: "",
+  year: 0,
+  rating: 0,
+}
+
+// 1. Fill object
+movie.title = "Lord of the Rings"
+movie["director"] = "Dwayne, The Rock, Johnson"
+movie.year = 2001
+movie["rating"] = 9.7
+
+// 2. Add watched property
+movie["watched"] = true
+
+// 3. Rating conditional
+function CheckRating(){
+  if (movie["rating"] >= 7){
+    console.log("This is a good movie")
+  } else {
+    console.log("Might not be the best movie")
+  }
+}
+
+// 4. Update rating function (maybe)
+function UpdateRating(newRating){
+  if(isNaN(newRating)){
+    console.log("Wrong Data type, Set rating to a #")
+  } else {
+    movie.rating = newRating
+  }
+}
+
+//Log base object
+console.log(movie)
+CheckRating()
+console.log("")
+
+//log updated rating and check rating
+UpdateRating(5.7654)
+console.log(movie)
+CheckRating()
+console.log("")
+
+//log with bad rating update (works, doesnt change rating if not a number)
+UpdateRating("John")
+console.log(movie)
+CheckRating()
